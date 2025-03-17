a584bdcf0086d0d7fe42f4bbdde8d270a358f119
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-03-17 17:47:47 +0100

    refactor: update database class style

--- models/database.py
+++ models/database.py
@@ -3,7 +3,9 @@ from mysql.connector import Error
 from dotenv import find_dotenv, load_dotenv
 from os import getenv
 
+
 class Database:
+    """ Class to manage the database. """
     def __init__(self):
         # Load the environment variables from .env
         self.dotenv_path = find_dotenv(".env")
@@ -16,7 +18,10 @@ class Database:
         self.db_password = getenv("PASSWORD")
 
     def connect(self, chosen_database=None):
-        """ Establish the database connection """
+        """
+        Establish the database connection.
+        :return: Database connection.
+        """
         try:
             conn = mysql.connector.connect(
                 host=self.db_host,
@@ -31,7 +36,11 @@ class Database:
             raise Exception(f"Database connection error: {e}")
     
     def create_database(self, database):
-        """create a database"""
+        """
+        Create a new database.
+        :param database: The database name.
+        :return: ∅
+        """
         with self.connect() as conn:  # with statement to automatically close the connection
             if conn:
                 cursor = conn.cursor()
@@ -39,58 +48,69 @@ class Database:
                 conn.commit()
 
     def create_table_client(self):
-        """create a table"""
+        """
+        Create the client table of the budget_buddy database.
+        :return: ∅
+        """
         with self.connect("budget_buddy") as conn:
             if conn:
                 cursor = conn.cursor()
                 cursor.execute("""
                     CREATE TABLE IF NOT EXISTS client(
-                    id_client INT AUTO_INCREMENT,
-                    last_name VARCHAR(100) NOT NULL,
-                    first_name VARCHAR(50) NOT NULL,
-                    email VARCHAR(100) NOT NULL,
-                    password VARCHAR(50) NOT NULL,
-                    PRIMARY KEY(id_client),
-                    UNIQUE(email)
-                    )""")
+                        id_client INT AUTO_INCREMENT,
+                        last_name VARCHAR(100) NOT NULL,
+                        first_name VARCHAR(50) NOT NULL,
+                        email VARCHAR(100) NOT NULL,
+                        password VARCHAR(50) NOT NULL,
+                        PRIMARY KEY(id_client),
+                        UNIQUE(email)
+                    )
+                    """)
                 conn.commit()
                 
     def create_account_table(self):
-        """create a table"""
+        """
+        Create the account table of the budget_buddy database.
+        :return: ∅
+        """
         with self.connect("budget_buddy") as conn:
             if conn:
                 cursor = conn.cursor()
                 cursor.execute("""
                     CREATE TABLE IF NOT EXISTS account(
-                    id_account INT AUTO_INCREMENT,
-                    IBAN VARCHAR(34) NOT NULL,
-                    amount DECIMAL(15,2) NOT NULL,
-                    creation_date DATE NOT NULL,
-                    id_client INT NOT NULL,
-                    PRIMARY KEY(id_account),
-                    UNIQUE(IBAN),
-                    FOREIGN KEY(id_client) REFERENCES client(id_client)
-
-                    )""")
+                        id_account INT AUTO_INCREMENT,
+                        IBAN VARCHAR(34) NOT NULL,
+                        amount DECIMAL(15,2) NOT NULL,
+                        creation_date DATE NOT NULL,
+                        id_client INT NOT NULL,
+                        PRIMARY KEY(id_account),
+                        UNIQUE(IBAN),
+                        FOREIGN KEY(id_client) REFERENCES client(id_client)
+                        )
+                        """)
                 conn.commit()
                 
     def create_transaction_table(self):
-        """create a table"""
+        """
+        Create the client transaction of the budget_buddy database.
+        :return: ∅
+        """
         with self.connect("budget_buddy") as conn:
             if conn:
                 cursor = conn.cursor()
                 cursor.execute("""
                     CREATE TABLE IF NOT EXISTS transaction(
-                    id_transaction INT AUTO_INCREMENT,
-                    id_origin_account INT NOT NULL,
-                    id_target_account INT NULL,
-                    amount DECIMAL(10, 2) NOT NULL,
-                    transaction_type VARCHAR(20) NOT NULL,
-                    transaction_date TIMESTAMP,
-                    PRIMARY KEY(id_transaction),
-                    FOREIGN KEY (id_origin_account) REFERENCES account(id_account),
-                    FOREIGN KEY (id_target_account) REFERENCES account(id_account)
-                    )""")
+                        id_transaction INT AUTO_INCREMENT,
+                        id_origin_account INT NOT NULL,
+                        id_target_account INT NULL,
+                        amount DECIMAL(10, 2) NOT NULL,
+                        transaction_type VARCHAR(20) NOT NULL,
+                        transaction_date TIMESTAMP,
+                        PRIMARY KEY(id_transaction),
+                        FOREIGN KEY (id_origin_account) REFERENCES account(id_account),
+                        FOREIGN KEY (id_target_account) REFERENCES account(id_account)
+                    )
+                    """)
                 conn.commit()
 
    
